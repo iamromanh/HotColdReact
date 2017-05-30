f9131fb38e3fc1f02bf4cdcfb919cd6aeb12be36
iamromanh <romanh99@gmail.com>
2017-05-30 14:47:19 -0400

    Add static components / static version of user interface

--- src/components/GuessBox.js
+++ src/components/GuessBox.js
@@ -9,9 +9,10 @@ export default function GuessBox(props) {
     <div>
       <section className='game'>
         <h2 id='user-feedback'>Make your Guess!</h2>
+        <form>
         <div className="field">
           <p className="control">
-            <input className="input text is-medium" type="text" name='userGuess' placeholder="Enter your Guess" />
+            <input className="input is-medium" id='userGuess' type="text" name='userGuess' placeholder="Enter your Guess" />
           </p>
         </div>
         <div className="field">
@@ -19,6 +20,11 @@ export default function GuessBox(props) {
             <input className='button' type="submit" id='guessButton' name='submit' value='Guess' />
           </p>
         </div>
+        </form>
+
+        <p className='guessCount'>Guess #<span id='count'>0</span>!</p>
+        <ul id='guestList' className='guessBox'>
+        </ul>
       </section>
     </div>
   )
