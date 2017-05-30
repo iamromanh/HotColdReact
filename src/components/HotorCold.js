fdd2dab9521eea522f9b681f852c06c8052ea681
iamromanh <romanh99@gmail.com>
2017-05-30 19:12:09 -0400

    Setup helper functions and state

--- src/components/HotorCold.js
+++ src/components/HotorCold.js
@@ -6,6 +6,26 @@ import GuessBox from './GuessBox';
 import Nav from './Nav'
 
 class HotorCold extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        guessesTaken: [],
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      guessesTaken: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(`Submitted your guess successfully ${this.state.guessesTaken}`)
+  }
+
   render() {
     return (
       <div className='HotorCold'>
